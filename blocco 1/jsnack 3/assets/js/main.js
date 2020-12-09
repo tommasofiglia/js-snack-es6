74fa300cc7f8352edc4955426a0059514fca1dbf
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-09 17:09:54 +0100

    jsnack 3 blocco 1 caricato

--- blocco 1/jsnack 3/assets/js/main.js
+++ blocco 1/jsnack 3/assets/js/main.js
@@ -1,3 +1,33 @@
-$(function () {
+/********************** CONSEGNA ************************
+Il software deve chiedere per 5 volte all’utente di inserire un
+numero. Il programma stampa la somma di tutti i numeri
+inseriti. Esegui questo programma in due versioni, con il for
+e con il while.
+******************************************************/
 
-});
+//Funzione creata con il for.
+const sumNumber = (hownumbers) => {
+  var sum = 0
+  for (var i = 0; i < hownumbers; i++) {
+    var userNumber = Number(prompt("Inserisci un numero"));
+    var sum = sum + userNumber;
+  }
+  return sum;
+};
+
+console.log(sumNumber(5));
+
+//Funzione creata con il while.
+const sumNumber2 = (hownumbers) => {
+  var sum2 = 0;
+  var i = 0;
+  while (i < hownumbers) {
+    var userNumber2 = Number(prompt("Inserisci un numero"));
+    var sum2 = sum2 + userNumber2;
+
+    i++;
+  }
+  return sum2;
+};
+
+console.log(sumNumber2(5));
